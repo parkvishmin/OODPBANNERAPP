75a8ea50f8710d7a14d7dcc0d144074e7691700b
VISHNU PRRASHATH M S <vishnu.0713jul@gmail.com>
2026-03-03 08:48:28 +0530

    Refactor UC1 to UC3_OopBanner with formatted output

--- UC3.java
+++ UC3.java
@@ -1,6 +1,11 @@
-public class UC1{
-
+public class UC3_OopBanner {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println(String.join("   ", " ***** ", " ***** ", "****** "));
+        System.out.println(String.join(" ", "*      *", "*      *", "*     *"));
+        System.out.println(String.join(" ", "*      *", "*      *", "*     *"));
+        System.out.println(String.join(" ", "*      *", "*      *", "* * * *"));
+        System.out.println(String.join(" ", "*      *", "*      *", "*     "));
+        System.out.println(String.join(" ", "*      *", "*      *", "*     "));
+        System.out.println(String.join("   ", " ***** ","****** * ", " "));
     }
 }
