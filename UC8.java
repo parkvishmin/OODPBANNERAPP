add94c3ed7f32b06ff92ab87e68d7e3b1acea9a8
VISHNU PRRASHATH M S <vishnu.0713jul@gmail.com>
2026-03-03 08:46:38 +0530

    Update UC8.java

--- UC8.java
+++ UC8.java
@@ -1,6 +1,29 @@
-public class UC1{
+import java.util.HashMap;
+import java.util.Map;
+
+public class UC8 {
+
+    static void displayBanner(String word, Map<Character, String[]> patterns) {
+        for (int i = 0; i < 7; i++) {
+            for (char c : word.toCharArray()) {
+                System.out.print(patterns.get(c)[i] + "  ");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        Map<Character, String[]> patterns = new HashMap<>();
+        patterns.put('O', new String[]{
+            " *** ", "*   *", "*   *", "*   *", "*   *", "*   *", " *** "
+        });
+        patterns.put('P', new String[]{
+            "**** ", "*   *", "*   *", "**** ", "*    ", "*    ", "*    "
+        });
+        patterns.put('S', new String[]{
+            " ****", "*    ", "*    ", " *** ", "    *", "    *", "**** "
+        });
+
+        displayBanner("OOPS", patterns);
     }
 }
