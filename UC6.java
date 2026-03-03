655521183de714bb7c610ad9cbd9acfbc3e92a34
VISHNU PRRASHATH M S <vishnu.0713jul@gmail.com>
2026-03-03 08:51:26 +0530

    Update UC6.java

--- UC6.java
+++ UC6.java
@@ -1,6 +1,29 @@
-public class UC1{
+public class UC6 {
+
+    static String[] O = {
+        " *** ",
+        "*   *",
+        "*   *",
+        "*   *",
+        "*   *",
+        "*   *",
+        " *** "
+    };
+
+    static String[] P = {
+        "**** ",
+        "*   *",
+        "*   *",
+        "**** ",
+        "*    ",
+        "*    ",
+        "*    "
+    };
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        // Print O O P
+        for (int i = 0; i < 7; i++) {
+            System.out.println(O[i] + "  " + O[i] + "  " + P[i]);
+        }
     }
 }
